4bcddcb8a52e1029bcf10f4e94fafe15ac230e06
Albert <40284104+CWMo@users.noreply.github.com>
2021-12-08 13:15:50 +0000

    Add logic of  creating document in mongodb to newBug

--- server.js
+++ server.js
@@ -1,16 +1,59 @@
 'use strict';
-const express = require("express"); 
+const express = require("express");
+const bodyParser  = require('body-parser');
+const mongoose = require("mongoose");
 
 let app = express();
-app.listen(3000,()=>{
-    console.log('App listening on port 3000')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log('App listening on port 3000')
 })
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log("after connect");
+
+const bugSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  desc: {type: String, required: true},
+  priority: {type: String, required: true},
+  assignedTo: String,
+  createdOn: {type:Date, default: Date.now},
+  updatedOn: {type:Date, default: Date.now},
+  status: {
+    type: String, 
+    enum: ['Open', 'In Progress', 'Closed', 'Cancelled', 'Suspended'], 
+    default: "Open"}
+});
+
+let Bug = mongoose.model('Bug', bugSchema);
+
 app.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/newbug.html');
   });
-  
+
+app.route('/api/bugs')
+  .post(function(req, res) {
+      const b = new Bug({
+        title: req.body.title, 
+        desc: req.body.desc, 
+        priority: req.body.priority, 
+        assignedTo: req.body.assignedTo || "" 
+        // the || "" part is necessary otherwise, the field may not be stored when undefined
+        });
+      b.save(function(err, data) {
+        if (err || !data) {
+          console.log(`error: ${err}`);
+          console.log(err);
+          res.json({error: err.messages})}
+        else {
+          console.log(`data: ${data}`);
+          return res.json(data)
+        }
+      });  
+  });
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
